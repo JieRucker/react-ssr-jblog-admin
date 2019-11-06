5f6d451931a8240051822e0bbffb80e88eab7362
JieRucker <1435255141@qq.com>
2019-11-06 20:33:12 +0800

    [fix] delete

--- config/dev.env.js
+++
@@ -1,6 +0,0 @@
-const api = require('./api');
-
-module.exports = {
-    NODE_ENV: '"development"',
-    api: JSON.stringify(api.dev)
-}
--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-const api = require('./api');
-
-module.exports = {
-    NODE_ENV: '"production"',
-    api: JSON.stringify(api.build)
-}
